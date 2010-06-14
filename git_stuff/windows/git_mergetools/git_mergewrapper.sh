4e934e6a329002af76f7b3dad004ccc7839bcd1a
Adrian Hodos <adi.hodos@gmail.com>
2010-06-14 03:21:47 +0300

    Fixed merge wrapper script for winmerge.

--- git_stuff/windows/git_mergetools/git_mergewrapper.sh
+++ git_stuff/windows/git_mergetools/git_mergewrapper.sh
@@ -1,2 +1,2 @@
 #!/bin/sh
-c:/opt/utils/winmerge/winmergeu.exe -e -u -wl -wr -maximize -dl "Mine" -dr "Theirs" "$1" "$3" "$4"
+c:/opt/utils/winmerge/winmergeu.exe -e -u -wl -wr -maximize -dl "Mine" -dr "Theirs" "$4"
