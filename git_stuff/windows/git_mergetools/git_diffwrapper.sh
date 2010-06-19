3a98f21473c9d540b97d865b160bf64bac62ebf2
Adrian Hodos <adi.hodos@gmail.com>
2010-06-19 13:49:28 +0300

    Tweaked diff scropt for windows.

--- git_stuff/windows/git_mergetools/git_diffwrapper.sh
+++ git_stuff/windows/git_mergetools/git_diffwrapper.sh
@@ -1,2 +1,2 @@
 #!/bin/bash
-c:/opt/utils/winmerge/winmergeu.exe -e -u -wl -wr -maximize -dl "Working copy" -dr "Base" "$2" "$5" | cat
+c:/opt/utils/winmerge/winmergeu.exe -e -u -wl -wr -maximize -dl "$2 - Repository" -dr "$5 - Working copy" "$2" "$5" | cat
